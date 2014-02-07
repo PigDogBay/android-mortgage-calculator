bccd2c6790e8556ca1b636fbcb2780d453121fbe
markpdbailey@gmail.com <markpdbailey@gmail.com@3172ce69-66ad-dc8c-3cfe-cb110ad2ee9c>
2014-02-07 16:45:31 +0000

    Added new about activity.

--- src/com/mpdbailey/mortgagecalculator/AboutActivity.java
+++ src/com/mpdbailey/mortgagecalculator/AboutActivity.java
@@ -1,45 +1,102 @@
 package com.mpdbailey.mortgagecalculator;
 
-import com.mpdbailey.utils.ActivityUtils;
+import com.pigdogbay.androidutils.mvp.BackgroundColorModel;
+import com.pigdogbay.androidutils.mvp.BackgroundColorPresenter;
+import com.pigdogbay.androidutils.mvp.IBackgroundColorView;
+import com.pigdogbay.androidutils.utils.ActivityUtils;
+import com.pigdogbay.androidutils.utils.PreferencesHelper;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class AboutActivity extends Activity
+
+public class AboutActivity extends Activity implements IBackgroundColorView
 {
+	BackgroundColorPresenter _BackgroundColorPresenter;
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_about);
-		RegisterIcon(R.id.facebookImg, R.string.facebookPage);
-		RegisterIcon(R.id.twitterImg, R.string.twitter);
-		RegisterIcon(R.id.androidImg, R.string.market);
-		RegisterIcon(R.id.bloggerImg, R.string.blogger);
-		RegisterIcon(R.id.pigdogbayImg, R.string.market);
-	}
 
-	private void RegisterIcon(int iconID, final int stringID)
-	{
-		ImageView img = (ImageView) findViewById(iconID);
-		img.setOnClickListener(new View.OnClickListener()
+		DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int width = displayMetrics.widthPixels/2;
+		
+        Button btn = (Button) findViewById(R.id.aboutBtnRate);
+        btn.setMinimumWidth(width);
+        btn.setOnClickListener(new OnClickListener() {
+			public void onClick(View v)
+			{
+				showWebPage(R.string.market_url);
+			}
+		});
+        btn = (Button) findViewById(R.id.aboutBtnFacebook);
+        btn.setMinimumWidth(width);
+        btn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				showWebPage(R.string.facebookPage);
+			}
+		});
+		((Button) findViewById(R.id.aboutBtnLegal)).setOnClickListener(new OnClickListener()
 		{
 			public void onClick(View v)
 			{
-				ShowWebPage(getString(stringID));
+				showLegal();
 			}
 		});
+		PreferencesHelper ph = new PreferencesHelper(this);
+		BackgroundColorModel bcm = new BackgroundColorModel(ph);
+		_BackgroundColorPresenter = new BackgroundColorPresenter(this,bcm);
+		_BackgroundColorPresenter.updateBackground();
+		
 	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		getMenuInflater().inflate(R.menu.menu_about, menu);
+		return true;
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		switch (item.getItemId())
+		{
+		case (R.id.menu_about_home):
+			finish();
+			break;
+		}
+		return true;
+	}	
 
-	private void ShowWebPage(String url)
+	private void showWebPage(int urlId)
 	{
 		try
 		{
-			ActivityUtils.ShowWebPage(this, url);
-		} catch (Exception e){}
+			ActivityUtils.ShowWebPage(this, getString(urlId));
+		}
+		catch (ActivityNotFoundException e)
+		{ 
+				Toast.makeText(this, getString(R.string.about_no_market_app), Toast.LENGTH_LONG)
+					.show();
+		}
+		
 	}
-
-
+	private void showLegal()
+	{
+		ActivityUtils.showInfoDialog(this, R.string.copyright_title, R.string.copyright);
+	}
+	@Override
+	public void setBackgroundColor(int id) {
+		ActivityUtils.setBackground(this, R.id.rootLayout, id);
+	}
 }

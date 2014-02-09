8c3766f5b363f88bf8c37cd8e05abc27a00edce5
markpdbailey@gmail.com <markpdbailey@gmail.com@3172ce69-66ad-dc8c-3cfe-cb110ad2ee9c>
2014-02-09 14:48:12 +0000

    add number picker code

--- src/com/mpdbailey/mortgagecalculator/CalculatorFragment.java
+++ src/com/mpdbailey/mortgagecalculator/CalculatorFragment.java
@@ -1,5 +1,8 @@
 package com.mpdbailey.mortgagecalculator;
 
+import com.pigdogbay.androidutils.usercontrols.NumberPicker;
+import com.pigdogbay.androidutils.usercontrols.NumberPickerValue;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,9 +11,24 @@ import android.view.ViewGroup;
 
 public class CalculatorFragment extends Fragment{
 	MortgagePresenter _MortgagePresenter;
+	MortgageModel _MortgageModel;
+	NumberPicker _MortgageNumberPicker; 
+	
+	public void setMortgageModel(MortgageModel model){
+		_MortgageModel = model;
+	}
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_calculator, container, false);
         return rootView;
+    }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+    	// TODO Auto-generated method stub
+    	super.onActivityCreated(savedInstanceState);
+    	_MortgageNumberPicker = (NumberPicker)getActivity().findViewById(R.id.CalculatorMortgagePicker);
+    	NumberPickerValue npv = new NumberPickerValue();
+    	npv.setValue(_MortgageModel.Mortgage);
+    	_MortgageNumberPicker.setNumberPickerValue(npv);
     }
 }

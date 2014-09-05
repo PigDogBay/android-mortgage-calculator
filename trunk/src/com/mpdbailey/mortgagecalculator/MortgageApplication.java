8a98894066a494bda0cdfeee3cd992a0a96b5e89
markpdbailey@gmail.com <markpdbailey@gmail.com@3172ce69-66ad-dc8c-3cfe-cb110ad2ee9c>
2014-09-05 15:28:54 +0000

    Added facebook, twitter links. Added standard tracker code

--- trunk/src/com/mpdbailey/mortgagecalculator/MortgageApplication.java
+++ trunk/src/com/mpdbailey/mortgagecalculator/MortgageApplication.java
@@ -1,11 +1,16 @@
 package com.mpdbailey.mortgagecalculator;
 
+import android.app.Activity;
 import android.app.Application;
+import android.support.v4.app.Fragment;
 
 import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
 
 public class MortgageApplication extends Application{
+	private static final String CATEGORY = "Mortgage Calculator";
+	private static final String PREFIX = "MC ";
 	private Tracker _Tracker;
 
 	synchronized Tracker getTracker(){
@@ -14,5 +19,19 @@ public class MortgageApplication extends Application{
 		}
 		return _Tracker;
 	}
+	public static void trackEvent(Activity activity, String category, String action, String label){
+		Tracker t = ((MortgageApplication)activity.getApplication()).getTracker();
+    	t.send(new HitBuilders.EventBuilder()
+		.setCategory(category)
+		.setAction(action)
+		.setLabel(label)
+		.build());
+	}
+	public static void trackEvent(Activity activity, String screenName, String label){
+		trackEvent(activity,CATEGORY, PREFIX+screenName, PREFIX+label);
+	}	
+	public static void trackEvent(Fragment fragment, String screenName, String label){
+		trackEvent(fragment.getActivity(), screenName, label);
+	}	
 
 }
